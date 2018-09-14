3be9a4e75e97745338230e3e71fcc8ec20bdde96
Andre Huang <huangand000@gmail.com>
2018-09-14 15:19:12 -0700

    Minor spacing changes

--- server.js
+++ server.js
@@ -8,26 +8,20 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 var request = require('request');
 
-
 app.get('/', function(req, res) {
-
     res.render('index');
-
 })
 
 app.post('/process', function(req, res) {
-
     var points = req.body.coordinates.split(",")
-    
     request('https://api.nasa.gov/planetary/earth/imagery/?lon='+points[0]+'&lat='+points[1]+'&date=2014-02-01&cloud_score=True&api_key=fU5PVIQKqs77NeWXbMbU2Oc8WuK3qlnpxVR08P0G', function(error, response, body) {
         if (error) {
             res.json({message: "Error"});
         } else {
             res.json({message: "Success", info: JSON.parse(body)});
         }
-    })
-
-})
+    });
+});
 
 app.listen(8000, function() {
  console.log("listening on port 8000");
